000b139f66e36e7e827661d6ec2103d150a73082
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2023-10-11 21:04:52 +0530

    add graphs

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,38 +1,140 @@
-import { BarChart } from '@mui/x-charts';
-import preactLogo from '../../assets/preact.svg';
-import './style.css';
+import { BarChart, LineChart, PieChart } from "@mui/x-charts";
+import { useComputed, useSignal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
+import "./style.css";
+
+const valueFormatter = (date: Date) =>
+  date.getHours() === 0
+    ? date.toLocaleDateString("fr-FR", {
+        month: "2-digit",
+        day: "2-digit",
+      })
+    : date.toLocaleTimeString("fr-FR", {
+        hour: "2-digit",
+      });
 
 export function Home() {
-	return (
-		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<BarChart  xAxis={[
-    {
-      id: 'barCategories',
-      data: ['bar A', 'bar B', 'bar C'],
-      scaleType: 'band',
-    },
-	
-  ]}
-  series={[
-    {
-      data: [2, 5, 3],
-    },
-  ]}
-  width={500}
-  height={300}></BarChart>
-		
-		</div>
-	);
+  const makeGraphs = (events) =>
+    events.data.map((event) => {
+      switch (event.type) {
+        case "pie":
+          return (
+            <div>
+              <h3>{event.title}</h3>
+              <PieChart
+                series={[
+                  {
+                    data: event.data.map((chunk, index) => {
+                      return {
+                        id: index,
+                        value: chunk.count,
+                        label: chunk.label,
+                      };
+                    }),
+                  },
+                ]}
+                width={500}
+                height={200}
+                className="pie-container"
+              />
+            </div>
+          );
+
+        case "line":
+          return (
+            <div>
+              <h3>{event.title}</h3>
+              <LineChart
+                xAxis={[
+                  {
+                    data: event.data.map((chunk, i) => new Date(2023, 10, i)),
+                    scaleType: "time",
+                    valueFormatter: valueFormatter,
+                  },
+                ]}
+                series={[
+                  {
+                    data: event.data.map((chunk) => chunk.count),
+                  },
+                ]}
+                width={500}
+                height={300}
+              />
+            </div>
+          );
+        case "bar-single":
+          return (
+            <div>
+              <h3>{event.title}</h3>
+              <BarChart
+                xAxis={[
+                  {
+                    scaleType: "band",
+                    data: event.data.map((chunk, i) => chunk.label),
+                  },
+                ]}
+                series={[
+                  {
+                    data: event.data.map((chunk) => chunk.count),
+                  },
+                ]}
+                width={1200}
+                height={400}
+              />
+            </div>
+          );
+        case "bar-multiple":
+          const series = [...new Set(event.data.map((e) => e.label))];
+
+          const seriesData = series.map((s) => {
+            return {
+              stack: "total",
+              data: event.data.filter((e) => e.label === s).map((e) => e.count),
+              label: s,
+            };
+          });
+
+          return (
+            <div>
+              <h3>{event.title}</h3>
+              <BarChart
+                xAxis={[
+                  {
+                    scaleType: "band",
+                    data: [...new Set(event.data.map((chunk, i) => chunk.key))],
+                  },
+                ]}
+                series={seriesData}
+                width={1200}
+                height={400}
+              />
+            </div>
+          );
+      }
+    });
+
+  const events = useSignal({ data: [] });
+  const graphs = useComputed(() => makeGraphs(events.value));
+
+  async function loadData() {
+    const response = await fetch("/data");
+    const data = await response.json();
+    console.log("data is", data);
+    events.value = data;
+  }
+
+  useEffect(() => {
+    loadData().then(() => console.log("done"));
+  }, []);
+
+  return <div class="home graph-container">{graphs.value}</div>;
 }
 
 function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+  return (
+    <a href={props.href} target="_blank" class="resource">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </a>
+  );
 }
